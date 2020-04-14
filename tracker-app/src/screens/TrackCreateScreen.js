345dd0c6cee6ebe128082d9f6dbc95af5178cbd5
harshthakur066 <harshthakur066@gmail.com>
2020-04-15 02:11:01 +0530

    reseting the state after saving and navigate to the the tack list screen

--- tracker-app/src/screens/TrackCreateScreen.js
+++ tracker-app/src/screens/TrackCreateScreen.js
@@ -25,7 +25,7 @@ const TrackCreateScreen = ({ isFocused }) => {
   const [err] = useLocation(isFocused || recording, callback);
   // console.log(isFocused);
   return (
-    <SafeAreaView forceInset={{ top: "always" }}>
+    <SafeAreaView style={{ flex: 1 }} forceInset={{ top: "always" }}>
       <Text h2>Create A Track</Text>
       <Map />
       {err ? <Text>Please enable location services</Text> : null}
